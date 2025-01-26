e32b96421dbcfd28bf46decb1296ac83847f6460
brandonlooo <161289686+brandonlooo@users.noreply.github.com>
2025-01-26 06:41:43 -0800

    PetrFindr Logo

--- hackathonfrontend/src/pages/HomePage.js
+++ hackathonfrontend/src/pages/HomePage.js
@@ -2,6 +2,7 @@ import { Box, Button } from "@mui/material";
 import { useNavigate } from "react-router";
 import { Link } from "react-router";
 import { Typography } from "@mui/material/";
+import logo from "../assets/pfl.png"; // Replace with your image path
 
 export default function HomePage() {
     let navigate = useNavigate();
@@ -18,10 +19,23 @@ export default function HomePage() {
       }}
     >
       <Box sx={{ display: "flex", flexDirection: "column", width: '20%', textAlign: 'center'}}>  
-        <Typography variant="h1" sx={{fontSize: "60px", paddingBottom: "20px"}}>PetrFindr</Typography>
+       
+      <Box
+          component="img"
+          src={logo}
+          alt="PetrFindr Logo"
+          sx={{
+            width: "200%", // Set image width
+            height: "auto", // Maintain aspect ratio
+            marginBottom: "20px", // Add spacing below the image
+            alignSelf: "center", // Center the image horizontally
+          }}
+        />
+       
+        {/* <Typography variant="h1" sx={{fontSize: "70px", paddingBottom: "20px"}}>PetrFindr</Typography> */}
         <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
-            <Button variant="contained" sx={{backgroundColor: 'primary.dark'}} onClick={() => {navigate('/project')}}>Go to Project</Button>
-            <Button variant="contained" component={Link} to='/team-bio'>Team Bio</Button>
+            <Button variant="contained" sx={{backgroundColor: 'primary.dark'}} onClick={() => {navigate('/project')}}>Find Location</Button>
+            <Button variant="contained" sx={{backgroundColor: 'primary.dark'}} component={Link} to='/team-bio'>Team Bio</Button>
         </Box>
       </Box>
     </Box>
